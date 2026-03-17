3310e9ca091ac8a17d0869fc5d8b0f496e24bef2
Danila Platonov <danilaplatonov209@gmail.com>
2026-03-17 12:56:45 +0300

    initial

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors({ 
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
     credentials: true 
 }));
